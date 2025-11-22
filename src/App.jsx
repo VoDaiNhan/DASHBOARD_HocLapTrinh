25f837e23a3a6fd4a8ac81ce1f325df29deebb66
TuanMinh22CT <minhgameripgelle@gmail.com>
2025-11-22 20:53:18 +0700

    delete and add

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Register from './pages/auth/Register';
 import ForgotPassword from './pages/auth/ForgotPassword';
 import ResetPassword from './pages/auth/ResetPassword';
 
-// Giảng viên imports
+// Giảng viên 
 import LayoutTeacher from './giangvien/components/Layout/Layout';
 import DashboardTeacher from './giangvien/pages/Dashboard/Dashboard';
 import ClassManagement from './giangvien/pages/ClassManagement/ClassManagement';
